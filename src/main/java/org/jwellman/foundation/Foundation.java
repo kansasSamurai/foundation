8ab433a581c510e4d9ce50da7ab9850d64c78897
Richard L. Wellman <rcwellman@yahoo.com>
2017-10-01 19:17:11 -0500

    Initial Commit (mmdealv2)

---
+++ src/main/java/org/jwellman/foundation/Foundation.java
@@ -0,0 +1,29 @@
+package org.jwellman.foundation;
+
+import javax.swing.JPanel;
+
+/**
+ * A micro-framework for Swing applications.
+ *
+ * Usage:
+ * (1) init() - initializes the Swing framework
+ * (2) useWindow()/useDesktop() - supply your user interface within a JPanel
+ *     and instantiate the supporting Swing containers.
+ * (3) showGUI() - make your user interface/JPanel visible;
+ *     most applications will have initialized all data models
+ *     and this will usually be the last method called in your startup() code.
+ *
+ * @author Rick Wellman
+ */
+public class Foundation extends Platinum {
+
+    private Foundation() {} // private constructor
+
+    private static Foundation f; // singleton
+
+    public static Foundation get() {
+        if (f == null) f = new Foundation();
+        return f;
+    }
+
+}
