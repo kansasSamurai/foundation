f1479b724eee9e9af4157b2872cfe18e383eca21
Rick Wellman <redsamurai_are-usingbitbucket@yahoo.com>
2020-01-11 22:31:10 -0600

    Updated to Java8

--- src/main/java/org/jwellman/foundation/Stone.java
+++ src/main/java/org/jwellman/foundation/Stone.java
@@ -1,9 +1,13 @@
 package org.jwellman.foundation;
 
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.swing.JDesktopPane;
@@ -20,36 +24,36 @@ import org.jwellman.foundation.swing.XInternalFrame;
 
 /**
  * The most basic of Swing initialization requirements.
- * 
- * Stone only supports a single application; 
+ *
+ * Stone only supports a single application;
  * in either single frame or desktop mode.
  *
  * @author rwellman
- * 
+ *
  */
 public class Stone {
 
 	/** The user's entry point UI in a JPanel */
 	// protected JPanel panel;
-	
+
 	/** A user interface context object */
 	private uContext context;
-	
+
 	/** Indicates desktop mode */
 	protected boolean isDesktop;
-	
+
 	/** Guards the init() method */
 	protected boolean isInitialized;
-	
+
 	/** Guards the useWindow() and useDesktop() methods so the first call "wins"; subsequent calls log a warning */
 	//protected boolean isAppModeChosen;
-	
+
 	/** The "controlling" JFrame; used in both modes */
 	protected XFrame externalFrame;
-	
+
 	/** The "main" internal frame used in desktop mode */
 	// protected XInternalFrame internalFrame;
-	
+
 	/** The JDesktopPane used in desktop mode */
 	private JDesktopPane desktop;
 
@@ -67,26 +71,26 @@ public class Stone {
 	/**
 	 * This is the workhorse of initializing the graphical "environment" in Swing;
 	 * note that it mainly consists of initializing the Look and Feel.
-	 * 
-	 * Note also that "initializing the environment" does not consist of 
+	 *
+	 * Note also that "initializing the environment" does not consist of
 	 * creating ANY actual Swing components -- that will come later
 	 * as you build your user interface.  i.e. this method does the
 	 * bootstrapping for you -- you get to focus on building the UI.
-	 * 
+	 *
 	 * Finally, note that it is enforced that the initialization occurs only
-	 * once -- any subsequent calls will log a warning but not actually do anything else. 
-	 * 
+	 * once -- any subsequent calls will log a warning but not actually do anything else.
+	 *
 	 * @param c the micro context
 	 */
 	protected final void _init(uContext c) {
-	
+
 	    if (isInitialized) {
 	    	System.out.print("WARN - init() has been called more than once...");
 	    	System.out.print("WARN - ... in a single app use case, this usually indicates a misuse of the API");
 	    	System.out.print("WARN - ... which may often result in unexpected/undesired behavior.");
 	    } else {
 	        isInitialized = true;
-	
+
 	        // Log the application classpath for debugging purposes
 	        System.out.println("----- Application Classpath -----");
 	        final ClassLoader cl = ClassLoader.getSystemClassLoader();
@@ -94,7 +98,19 @@ public class Stone {
 	        for (URL url: urls){
 	            System.out.println(url.getFile());
 	        }
-	
+
+	        // Log the system fonts available for debugging purposes
+	        final GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+	        final Font[] fonts = ge.getAllFonts();
+	        for (Font font : fonts) {
+	            System.out.print("FONT: " + font.getFontName() + " : ");
+	            System.out.println(font.getFamily());
+	        }
+
+	        @SuppressWarnings("unused")
+			Map<?, ?> desktopHints =
+       	        (Map<?, ?>) Toolkit.getDefaultToolkit().getDesktopProperty("awt.font.desktophints");
+
 	        // Apply anti-aliasing for better rendering (particulary fonts)
 	        // The following may have some subtle system dependent behavior:
 	        // http://stackoverflow.com/questions/179955/how-do-you-enable-anti-aliasing-in-arbitrary-java-apps
@@ -108,16 +124,16 @@ public class Stone {
 		        System.setProperty("swing.aatext", "false");
 		        System.out.println("Anti-alias settings:  off/false");
 	        }
-	        
+
 	        // Make sure our window decorations come from the look and feel.
 	        JFrame.setDefaultLookAndFeelDecorated(true);
-	
+
 	        // Save the context (or create one by default)
 	        context = (c != null) ? c : uContext.createContext();
-	
+
 	        // Conditionally apply context settings...
-	        if (context.getThemeProvider() != null) { 
-	        	context.getThemeProvider().doTheme(); 
+	        if (context.getThemeProvider() != null) {
+	        	context.getThemeProvider().doTheme();
 	        }
 
         // Prefer Nimbus over default look and feel.
@@ -130,7 +146,7 @@ public class Stone {
 		            Properties props = new Properties();
 
 		           // LAF_NIMBUS; LAF_WEB; LAF_MATCHES_SETTING;
-          			final int version = LAF_MATCHES_SETTING; 
+          			final int version = LAF_MATCHES_SETTING;
                     switch (version) {
                         case LAF_MATCHES_SETTING:
                             UIManager.setLookAndFeel(info.getClassName());
@@ -164,13 +180,13 @@ public class Stone {
         }
 
     }
-    
+
 	} // end method
 
 	/**
 	 * Given an instance of JPanel, return an IWindow object compatible
 	 * with a "desktop" user experience (i.e. a JInternalFrame).
-	 * 
+	 *
 	 * @param ui
 	 * @return
 	 */
@@ -178,34 +194,34 @@ public class Stone {
 		if (ui == null) {
 			throw new RuntimeException("FATAL - JPanel cannot be null");
 		}
-		
+
         //panel = ui; // Store a reference to the JPanel
         // TODO may have to deprecate/remove the "panel" reference
         // because there may eventually be many panels
 
 	    final XInternalFrame internalFrame = new XInternalFrame("Your UI", true, true, true, true);
-        // TODO figure out the best way to size the internal frame (probably the context); this is just a stopgap 
+        // TODO figure out the best way to size the internal frame (probably the context); this is just a stopgap
         internalFrame.setBounds(10, 10, 225, 125);
         // internalFrame.setVisible(true); // [B]
         internalFrame.add(ui);
-        // These are the logical default values; maximizable can be later altered... 
+        // These are the logical default values; maximizable can be later altered...
         // TODO technically, closable can be altered too but in a single app context,
-        // it should probably be prevented somehow. 
+        // it should probably be prevented somehow.
         internalFrame.setMaximizable(false);
         internalFrame.setClosable(false);
-        
+
         // Adding this to a desktop has been moved to showGUI()
         // desktop.add(internalFrame); // this does NOT make the internal frame visible
 
         this.initializeOtherWindows();
 
-	    return internalFrame; // frame;	
+	    return internalFrame; // frame;
 	} // end method
 
 	/**
 	 * Given an instance of JPanel, return an IWindow object compatible
 	 * with a window-based user experience (i.e. a JFrame).
-	 * 
+	 *
 	 * @param ui
 	 * @return
 	 */
@@ -213,9 +229,17 @@ public class Stone {
 		if (ui == null) {
 			throw new RuntimeException("FATAL - JPanel cannot be null");
 		}
-	
+
 	    // Create the JFrame
 	    final XFrame ajframe = new XFrame("Your App -- powered by the Foundation API");
+
+        // ... possibly update the frame title
+        if (context.getDesktopTitle() != null) ajframe.setTitle(context.getDesktopTitle());
+
+        // ... only set the close operation when not in desktop mode
+        // TODO this default is only valid in a non-desktop, single window
+        // user interface; if this method is called for any second, third, etc.
+        // windows then this default probably doesn't apply; what to do then?
 	    if (context.isDesktopMode()) {
 	    } else {
 		    ajframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -223,69 +247,69 @@ public class Stone {
 	    // frame.setSize(450, 250); // [D]
 	    ajframe.add(ui);
 
-	    return ajframe;		
+	    return ajframe;
 	}
 
 	/**
-	 * 
+	 *
 	 * @param main
 	 */
 	public void registerDesktopWindow(IWindow main) {
 		if (main instanceof XFrame) {
-			externalFrame = (XFrame) main;			
+			externalFrame = (XFrame) main;
 		} else {
 			throw new RuntimeException("Invalid window registered as desktop; must be an instance of XFrame");
 		}
 	}
-	
+
 	/**
-	 * A convenience method for calling showGUI() when you 
+	 * A convenience method for calling showGUI() when you
 	 * only have one IWindow instance.
-	 * 
+	 *
 	 * @param window
 	 */
 	public void showGUI(IWindow window) {
-	
+
 		final List<IWindow> list = new ArrayList<>();
 		if (window != externalFrame) {
-			list.add(window);			
+			list.add(window);
 		}
-		
+
 		this.showGUI(list);
 	}
 
 	/**
 	 * This is where your application is finally "visible" to the user.
-	 * 
+	 *
 	 * The logic of this method follows two entry conditions:
 	 * 1) Has this been called already? which should only be valid if
 	 *    you are purposely running in a desktop mode.
-	 *    
+	 *
 	 * 2) Has this **not** been called already? which should only be valid
 	 *    if your are either just running a standalone application **or**
 	 *    if you are creating a generic desktop to host multiple applications
 	 *    (such as jPAD).
-	 *    
+	 *
 	 * - If the desktop mode has been chosen, create the external frame (JFrame)
 	 * - If the window mode has been chosen, create the external frame (JFrame)
-	 * 
-	 * 
+	 *
+	 *
 	 */
 	public void showGUI(final List<IWindow> windows) {
-	
+
 		if (!isInitialized) {
 			throw new RuntimeException("Cannot call showGUI() until either useWindow() or useDesktop() is called.");
 		}
-	
+
         // Create the JFrame
 		if (externalFrame == null) {
 			// We have not registered a desktop/main so create one
-	        externalFrame = new XFrame(context.getDesktopTitle());	        
+	        externalFrame = new XFrame(context.getDesktopTitle());
 		} else {
 			// We have registered a desktop so use it
-			
+
 		}
-        
+
     // Start the GUI on the Event Dispatch Thread (EDT)
     javax.swing.SwingUtilities.invokeLater(
         new Runnable() { @Override public void run() {
@@ -293,7 +317,7 @@ public class Stone {
         	if (context.isDesktopMode()) {
         		if (context.getDesktopProvider() == null) {
         	        desktop = new JDesktopPane(); // a specialized layered pane
-        	        desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE); // Make dragging a little faster but perhaps uglier.        			
+        	        desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE); // Make dragging a little faster but perhaps uglier.
         	        externalFrame.setContentPane(desktop);
         		} else {
         			desktop = context.getDesktopProvider().doCustomDesktop(externalFrame);
@@ -306,14 +330,14 @@ public class Stone {
                 }
 
         	}
-        	
+
 	        // TODO The jPAD security manager doesn't like this line
 	        // but other apps without jpad might... review this design
 	        // externalFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        	        
+
             // Display the window.
             // externalFrame.pack(); // [A] [E]
-            externalFrame.setSize(context.getDimension());			
+            externalFrame.setSize(context.getDimension());
             externalFrame.setLocationRelativeTo(null); // [C]
             externalFrame.setVisible(true);
 
@@ -322,19 +346,19 @@ public class Stone {
 
 	    /* All the other windows have been added to the desktop
 	     * but they have not been made visible; make them visible now.
-	     * 
+	     *
 	     * For possible performance reasons, open each subwindow in a new EDT
 	     */
 	    for (final IWindow w : windows) {
-	    	
+
 	        // Start the GUI on the Event Dispatch Thread (EDT)
 	        javax.swing.SwingUtilities.invokeLater(
 	            new Runnable() { @Override public void run() {
 	                w.pack();
-	                w.setVisible(true);            	
+	                w.setVisible(true);
 	            }} );
 	    }
-	
+
 	} // end method
 
 	public JDesktopPane getDesktop() {
@@ -343,8 +367,8 @@ public class Stone {
 
 /* ========== Footnotes =====================================================
 [A] The swing documentation says that pack() makes the frame "displayable"
-    I originally thought that "displayable" meant "visible" but it doesn't 
-    (or at least it doesn't work that way).  
+    I originally thought that "displayable" meant "visible" but it doesn't
+    (or at least it doesn't work that way).
     A:  "displayable" means that the component,
         or its root container, has a native-peer.
 [B] We intentionally defer showing the internal frame until the user/designer
